0e9668a18de2fa4f9e59f4ebaf6635cccdba4dbd
Kunal Kamble <kkamble@I-FLEX.COM>
2024-03-28 10:14:26 -0400

    deploy_copy

--- apps/home.py
+++ apps/home.py
@@ -4,30 +4,53 @@ from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores.chroma import Chroma
 from pypdf import PdfReader
 from langchain.memory import ConversationBufferMemory
-from langchain.chains import RetrievalQA
+from langchain.chains import ConversationalRetrievalChain
 from langchain_community.chat_models import ChatGooglePalm
 from htmlTemplates import css
 from PIL import Image
 # from dotenv import load_dotenv
+from langchain_core.prompts import (PromptTemplate)
 
 CHROMA_DB_PATH = "./chroma_vector_database"
 
 # load_dotenv()
 
+def get_prompt_template():
+    template = """
+        You are helpful, respectful and honest assistant. Always answer as helpfully as possible, while being safe.
+        Use the given Context and History enclosed with backticks to answer the question at the end.
+        Don't try to make up an answer, if you don't know, just say that you don't know.
+
+        History: {chat_history}
+
+        Context: {context}
+
+        HumanMessage: {question}
+        AIMessage: 
+        """
+    return PromptTemplate(template=template, input_variables=["chat_history", "context", "question"])
+
+
 def get_conversation_chain():
     # for RetrievalQA
-    memory = ConversationBufferMemory(memory_key="chat_history", output_key='result', return_messages = True,
-                                      return_source_documents=True)
-    # for ConversationalRetrievalChain
-    # memory = ConversationBufferMemory(memory_key="chat_history", output_key='answer', return_messages=True,
-    #                                   return_source_documents=True)
-    # llm = load_hugging_face_llm()
-    llm = ChatGooglePalm(temprature = 0.5, model_kwargs={"max_length": 200})
+    llm = ChatGooglePalm(temprature = 0.1, model_kwargs={"max_length": 200})
     # llm = GoogleGenerativeAI(model="gemini-pro", temprature=0.5)
-    return RetrievalQA.from_llm(llm=llm, retriever=vector_store.as_retriever(),
-                                memory = memory, verbose= True, return_source_documents=True)
-    # return ConversationalRetrievalChain.from_llm(llm=llm, retriever=vector_store.as_retriever(),
-    #                             memory = memory, verbose= True, return_source_documents=True)
+
+    # memory = ConversationBufferMemory(memory_key="chat_history", output_key='result', return_messages = True,
+    #                                   return_source_documents=True)
+    # return RetrievalQA.from_llm(llm=llm, retriever=vector_store.as_retriever(),
+    #                             memory = memory, verbose= True, return_source_documents=True, )
+
+    # for ConversationalRetrievalChain
+    memory = ConversationBufferMemory(memory_key="chat_history", output_key='answer', return_messages=True,
+                                      return_source_documents=True)
+    # llm = load_hugging_face_llm()
+    return ConversationalRetrievalChain.from_llm(llm=llm, 
+                                                 retriever=vector_store.as_retriever(),
+                                                 memory = memory, 
+                                                 chain_type = "stuff",
+                                                 verbose= True, 
+                                                 return_source_documents=True)
 
 
 
@@ -42,25 +65,35 @@ def store_documents_in_database(docs):
         # to use own embedding
         # embeddings = HuggingFaceEmbeddings(model_name='google/flan-t5-xxl',model_kwargs={'device': 'cpu'})
         # db = st.session_state.vectordb.from_texts(texts = chunks)
-        vector_store.from_texts(persist_directory= CHROMA_DB_PATH, texts = chunks,
+        vector_store.from_texts(texts = chunks,
                                 embedding=embedding_function)
 
 
 def handle_user_questions(query):
     # for RetrievalQA
-    output = st.session_state.conversation_chain({"query": query})
+    # output = st.session_state.conversation_chain({"query": query})
     # for ConversationalRetrievalChain
-    # output = st.session_state.conversation_chain({"question": query})
-    # print(output)
-    st.session_state.chat_history=output["chat_history"]
-    for i, message in enumerate(st.session_state.chat_history):
-        if i%2 == 0:
-            # st.write(user_template.replace("{{MSG}}",message.content), unsafe_allow_html=True)
-            st.chat_message("user", avatar=Image.open('./images/chat_user.jpeg')).write(message.content)
-        else:
-            # st.write(bot_template.replace("{{MSG}}",message.content), unsafe_allow_html=True)
-            st.chat_message("user", avatar=Image.open('./images/ai_robot.jpg')).write(message.content)
-            # st.chat_message("user", avatar="🤖").write(message.content)
+    try:
+        output = st.session_state.conversation_chain.invoke({"question": query})
+    except:
+        output = None
+        print("Error while getting response")
+
+    if output:
+        st.session_state.chat_history=output["chat_history"]
+        print(st.session_state.chat_history)
+        for i, message in enumerate(st.session_state.chat_history):
+            if i%2 == 0:
+                # st.write(user_template.replace("{{MSG}}",message.content), unsafe_allow_html=True)
+                st.chat_message("user", avatar=Image.open('./images/chat_user.jpeg')).write(message.content)
+            else:
+                # st.write(bot_template.replace("{{MSG}}",message.content), unsafe_allow_html=True)
+                st.chat_message("assistant", avatar=Image.open('./images/ai_robot.jpg')).write(message.content)
+                # st.chat_message("user", avatar="🤖").write(message.content)
+    else:
+        st.chat_message("user", avatar=Image.open('./images/chat_user.jpeg')).write(query)
+        st.chat_message("assistant", avatar=Image.open('./images/ai_robot.jpg')).write("I don't know the answer.")
+
 
 st.set_page_config(page_title="Q and A using Documents", page_icon=":book:")
 st.write(css, unsafe_allow_html= True)
@@ -68,8 +101,7 @@ embedding_function = HuggingFaceEmbeddings(model_name="sentence-transformers/all
 
 
 with st.spinner('Initializing database...'):
-    vector_store = Chroma(persist_directory=CHROMA_DB_PATH,
-                          embedding_function = embedding_function)
+    vector_store = Chroma(embedding_function = embedding_function)
     # vector_retriever = st.session_state.vectordb.as_retriever()
 
 if "conversation_chain" not in st.session_state:
