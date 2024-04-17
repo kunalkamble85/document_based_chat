977329aca547e64d172fb6a8249b870aef5a1e57
Kunal Kamble <kkamble@I-FLEX.COM>
2024-04-17 15:53:58 -0400

    adding 2 use cases

--- utils/langchain_utils.py
+++ utils/langchain_utils.py
@@ -1,3 +1,6 @@
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_community.document_loaders import PyPDFLoader, TextLoader, Docx2txtLoader, CSVLoader, UnstructuredExcelLoader
 import traceback
 import tempfile
 import os
@@ -10,7 +13,6 @@ from langchain.chains import ConversationChain
 from langchain_community.chat_models import ChatGooglePalm
 from langchain_google_genai import ChatGoogleGenerativeAI
 import kuzu
-import requests
 
 def get_prompt_template():
     template = """
@@ -89,6 +91,14 @@ def get_prompt_summary_task(input, max_tokens):
         """
     return template
 
+
+def clear_vectordb(embedding_function):
+    vector_store = Chroma(persist_directory=st.session_state.CHROMA_DB_PATH, embedding_function = embedding_function)
+    for collection in vector_store._client.list_collections():
+        ids = collection.get()['ids']
+        print('REMOVE %s document(s) from %s collection' % (str(len(ids)), collection.name))
+        if len(ids): collection.delete(ids)
+
 def process_documents(file, num_rows):
     with tempfile.NamedTemporaryFile(delete=False) as temp_file:
         temp_file.write(file.read())
@@ -113,6 +123,68 @@ def process_documents(file, num_rows):
     print(synthetic_data.head())
     return synthetic_data
     
+def get_text_from_documents(file):
+    text = ""
+    try:
+        file_extension = os.path.splitext(file.name)[1]
+        print(file_extension)
+        with tempfile.NamedTemporaryFile(delete=False) as temp_file:
+            temp_file.write(file.read())
+            temp_file_path = temp_file.name
+        if file_extension == ".pdf":
+            loader = PyPDFLoader(temp_file_path)
+        elif file_extension == ".txt":
+            loader = TextLoader(temp_file_path)
+        elif file_extension == ".doc" or file_extension == ".docx":
+            loader = Docx2txtLoader(temp_file_path)
+        if loader:
+            for doc in loader.load():
+                text += "\n" + doc.page_content
+            os.remove(temp_file_path)
+            print(f"Got the content: {file}")
+    except:
+        print(traceback.format_exc())
+        print(f"Error while loading file: {file}")    
+    return text
+
+def store_documents_in_database(docs):
+    text = []
+    for file in docs:
+        try:
+            file_extension = os.path.splitext(file.name)[1]
+            print(file_extension)
+            with tempfile.NamedTemporaryFile(delete=False) as temp_file:
+                temp_file.write(file.read())
+                temp_file_path = temp_file.name
+
+            if file_extension == ".pdf":
+                loader = PyPDFLoader(temp_file_path)
+            elif file_extension == ".csv":
+                loader = CSVLoader(temp_file_path)
+            elif file_extension == ".txt":
+                loader = TextLoader(temp_file_path)
+            elif file_extension == ".doc" or file_extension == ".docx":
+                loader = Docx2txtLoader(temp_file_path)
+            elif file_extension == ".xls" or file_extension == ".xlsx":
+                loader = UnstructuredExcelLoader(temp_file_path)
+            if loader:
+                text.extend(loader.load())
+                os.remove(temp_file_path)
+            print(f"Saved file to vector database: {file}")
+        except:
+            print(traceback.format_exc())
+            print(f"Error while loading file: {file}")
+
+        text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=100, separators=["\n"], length_function=len)
+        chunks = text_splitter.split_documents(text)
+        # to use own embedding
+        # embeddings = HuggingFaceEmbeddings(model_name='google/flan-t5-xxl',model_kwargs={'device': 'cpu'})
+        # db = st.session_state.vectordb.from_texts(texts = chunks)
+        # clear_vecotrdb(embedding_function)
+        embedding_function = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+        st.session_state.vector_store = Chroma.from_documents(persist_directory=st.session_state.CHROMA_DB_PATH, documents = chunks, embedding=embedding_function)
+
+import requests
 
 def get_test_cases(input, option):
     API_URL = "https://api-inference.huggingface.co/models/mistralai/Mistral-7B-Instruct-v0.2"
