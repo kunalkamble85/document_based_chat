6a42580010d5b158e052fffce47e30c2f83cb569
Kunal Kamble <kunalkamble85@gmail.com>
2024-12-30 12:28:03 -0500

    adding charts

--- pages/8_Talk_To_Database.py
+++ pages/8_Talk_To_Database.py
@@ -2,6 +2,8 @@ import streamlit as st
 from utils.oci_utils import *
 from PIL import Image
 from utils.langchain_utils import get_data_from_chat_db, display_sidebar
+from pandas.api.types import is_numeric_dtype
+import matplotlib.pyplot as plt
 
 st.set_page_config(page_title="Talk to Database", page_icon=":book:", layout="wide")
 display_sidebar()
@@ -64,6 +66,41 @@ for message in st.session_state["chat_db_messages"]:
             st.dataframe(data = df, hide_index=True)
         # st.write(message["content"])
 
+def show_data(df):
+    if df is not None:
+        columns = df.columns
+        df = df[:100]
+        width = 300 
+        height = 300
+
+        x_axis = columns[0]
+        numeric_columns = [col for col in columns if is_numeric_dtype(df[col]) and x_axis!=col]
+
+        df_grouped = df.groupby(x_axis).sum()
+
+        if len(numeric_columns) > 0:
+            tab0, tab1, tab2, tab3, tab4= st.tabs(["Data", "Line Chart", "Bar Chart", "Pie Chart", "Scatter Chart"])
+            with tab0:
+                st.title("Data")
+                st.dataframe(data = df, hide_index=True)
+            with tab1:
+                st.title("Line Chart")
+                st.line_chart(df_grouped[numeric_columns], width=width, height=height, use_container_width=True)
+            with tab2:
+                st.title("Bar Chart")
+                st.bar_chart(df_grouped[numeric_columns], stack=False)
+            with tab3:
+                st.title("Pie Chart")            
+                fig, ax = plt.subplots()             
+                df_grouped[numeric_columns[0]].plot.pie(autopct='%1.1f%%', ax=ax)
+                st.pyplot(fig, use_container_width=False)
+            with tab4:
+                st.title("Scatter Chart")
+                st.scatter_chart(df_grouped[numeric_columns])            
+        else:
+            st.dataframe(data = df, hide_index=True)
+
+
 if user_query := st.chat_input("Enter your question here") or sample_selected:
     if sample_selected: user_query = sample_query
     st.session_state["chat_db_messages"].append({"role":"user","content":user_query})
@@ -72,10 +109,10 @@ if user_query := st.chat_input("Enter your question here") or sample_selected:
         st.session_state["chat_db_history"].append({"role":"user","content":user_query})
         response, df = handle_conversation()
         st.session_state["chat_db_history"].append({"role":"assistant","content":response})
-        with st.expander("SQL query", expanded=True):            
+        with st.expander("SQL query", expanded=False):            
             st.chat_message("assistant", avatar=Image.open('./images/ai_robot.jpg')).write(response)
         if df is not None:
-            st.dataframe(data = df, hide_index=True)
+            show_data(df)
         else:
             st.info("No Data found.")
         st.session_state["chat_db_messages"].append({"role":"assistant","content":(response, df)})
